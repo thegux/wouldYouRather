77d667c75a4faebc3f46c5c0797bd53acb2946be
Gabriel Bezerra <gabrielsrbezerra@gmail.com>
2020-03-29 17:21:45 -0300

    connected logout component to store

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,46 +2,52 @@ import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {Nav} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import {setAuthedUser} from '../Actions/authedUser'
 
 class NavComponent extends Component {
+
+  constructor(props){
+    super(props)
+    this.handleLogOut = this.handleLogOut.bind(this)
+  }
+
+  handleLogOut(){
+    this.props.dispatch(setAuthedUser(null))
+  }
 
   render(){
     
       return (
         <Nav activeKey="/home" className="justify-content-end">
-          
-        {this.props.loggedUser &&
-
-        <Fragment>
-
           <Nav.Item>
-            <Link to={'/'+ this.props.loggedUser.id +'/home'}>Home</Link>
+            <Link to={'/home'}>Home</Link>
           </Nav.Item>
 
           <Nav.Item>
-            <Link to={'/'+ this.props.loggedUser.id +'/newQuestion'}>New Question</Link>
+            <Link to={'/add'}>New Question</Link>
           </Nav.Item>
 
           <Nav.Item>
-            <Link to={'/'+ this.props.loggedUser.id +'/leaderBoard'}>Leader Board</Link>
+            <Link to={'/leaderBoard'}>Leader Board</Link>
           </Nav.Item>
+          {this.props.authedUser &&
+                  <Fragment>
+                    <Nav.Item>
+                    <p>
+      
+                            <img
+                            src={this.props.loggedUser.avatarURL} 
+                            alt={this.props.loggedUser.name + ' logo'}/>
+      
+                            Hi, {this.props.loggedUser.name}</p>
+                </Nav.Item>
+      
+                <Nav.Item onClick={this.handleLogOut}>
+                  <Link to="/">Logout</Link>
+                </Nav.Item>
+                </Fragment>  
+            } 
 
-          <Nav.Item>
-              <p>
-
-                      <img style={{width:'30px', height:'30px', borderRadius: '15px', marginRight: '10px'}}
-                      src={this.props.loggedUser.avatarURL} 
-                      alt={this.props.loggedUser.name + ' logo'}/>
-
-                      Hi, {this.props.loggedUser.name}</p>
-          </Nav.Item>
-
-          <Nav.Item>
-            <Link to="/">Logout</Link>
-          </Nav.Item>
-
-        </Fragment>   
-  }
       </Nav>
     )
  
@@ -54,7 +60,10 @@ class NavComponent extends Component {
 function mapStateToProps({users, authedUser}){
   const usersArray = Object.keys(users).map((u) => users[u])
   const loggedUser = usersArray.filter((u) => u.id === authedUser)[0]
-  return {loggedUser}
+  return { 
+    loggedUser,
+    authedUser
+  }
 }
 
 
